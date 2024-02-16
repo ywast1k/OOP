4d8115d6ab6e5a94991c0a9261a8574c829569ee
Ywast1k <kbynevskij@yandex.ru>
2024-02-16 15:44:52 +0100

    HW#1 No display

--- seminar#1/seminar1/src/App.java
+++ seminar#1/seminar1/src/App.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import domen.Bottle;
+import domen.HotDrink;
 import domen.Product;
 import services.Display;
 import services.Holder;
@@ -17,19 +18,28 @@ public class App {
             );
         Product product2 = new Product(
             2, "chocolate", 55.23, 10
-        );
+            );
         Product product3 = new Product(
             3, "nuts", 222.55, 15
             );
         Bottle bottle1 = new Bottle(
             4, "cola", 50, 11, 0.5
-                );
-               
+            );
+
+        HotDrink HotDrink1 = new HotDrink(
+            5, "cacao", 35, 16, 95 
+            );
+        HotDrink HotDrink2 = new HotDrink(
+            6, "starbax", 45, 17, 95 
+            );
+        
             List<Product> ourList = new ArrayList();
             ourList.add(product1);
             ourList.add(product2);
             ourList.add(product3);
             ourList.add(bottle1);
+            ourList.add(HotDrink1);
+            ourList.add(HotDrink2);
             
         
             
--- seminar#1/seminar1/src/domen/HotDrink.java
+++ seminar#1/seminar1/src/domen/HotDrink.java
@@ -1,5 +1,25 @@
 package domen;
 
-public class HotDrink {
-    
+public class HotDrink extends Product {
+    private int temperature;
+
+    public HotDrink(
+        long id, String name, double price,
+         int place, int temperature) {
+        super(id, name, price, place);
+        this.temperature = temperature;
+    }
+
+    public int getTemperatur() {
+        return temperature;
+    }
+
+    public void setTemperatur(int temperature) {
+        this.temperature = temperature;
+    }
+
+    @Override
+    public String toString() {
+        return "HotDrink [" + super.toString() + "temperature= " + temperature + "]";
+    }    
 }
